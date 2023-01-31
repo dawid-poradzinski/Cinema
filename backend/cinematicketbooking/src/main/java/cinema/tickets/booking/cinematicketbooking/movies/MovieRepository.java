0b5a8dd3b21ee4e0c9717aaee3c8e63a0fec2162
dawid-poradzinski <dawid-poradzinski@gmail.com>
2023-01-31 15:49:53 +0100

    First push after a long break because of collage. Added new methods and worked on SQL

--- backend/cinematicketbooking/src/main/java/cinema/tickets/booking/cinematicketbooking/movies/MovieRepository.java
+++ backend/cinematicketbooking/src/main/java/cinema/tickets/booking/cinematicketbooking/movies/MovieRepository.java
@@ -2,9 +2,9 @@ package cinema.tickets.booking.cinematicketbooking.movies;
 
 import org.springframework.data.repository.CrudRepository;
 
-// interface dla JPA, aby tworzyć podstawowe funkcje CR(zmienna, typ id)
-public interface MovieRepository extends CrudRepository<Movie, String>
+// interface dla JPA, aby tworzyć podstawowe funkcje CR(zmienna, typ id) wbudowane komędy SQL
+// hold movie records
+public interface MovieRepository extends CrudRepository<Movie, Integer>
 {
-    
-    
+    Movie findByTitle(String title);
 }
